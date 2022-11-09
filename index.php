d7652eadc70afc72d3db229f6a02be562eddddf1
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-11-09 16:14:01 +0430

    reformat

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOW-OLD-IS</title>
+
+    <link href="library/css/output.css" rel="stylesheet" />
+
+</head>
+
+<body>
+    <?php
+    // $db = new SQLite3('tech_db.db');
+    // $results = $db->query('SELECT tech_name FROM technologies');
+
+    // while ($row = $results->fetchArray()) {
+    //     print($row['tech_name']);
+    //     print("<br/>");
+    // }
+    ?>
+</body>
+<script>
+    
+</script>
+</html>
