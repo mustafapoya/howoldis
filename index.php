0d9e49b9d29d00fcd80eab1637fd724a0ab165bb
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-11-10 15:29:40 +0430

    create and update the navbar

--- index.php
+++ index.php
@@ -7,21 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOW-OLD-IS</title>
     <link rel="stylesheet" href="library/plugin/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="library/plugin/font-awesome/css/fontawesome.min.css" />
+    <link rel="stylesheet" href="library/plugin/font-awesome/css/brands.min.css" />
+    <link rel="stylesheet" href="library/plugin/font-awesome/css/solid.min.css" />
+
     <link rel="stylesheet" href="library/css/style.css" />
+
     <script src="library/plugin/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="library/js/script.js"></script>
 </head>
 
 <body>
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <symbol id="home" viewBox="0 0 16 16">
-            <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
-        </symbol>
-        <symbol id="people-circle" viewBox="0 0 16 16">
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-        </symbol>
-    </svg>
     <header>
         <div class="px-3 py-2 text-bg-dark">
             <div class="container">
@@ -33,17 +29,19 @@
                     <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
                         <li>
                             <a href="#" class="nav-link text-secondary">
-                                <svg class="bi d-block mx-auto mb-1" width="24" height="24">
-                                    <use xlink:href="#home" />
-                                </svg>
+                                <span class="fa-solid fa-house-user" style="width: 24px; height: 24px;"></span>
                                 Home
                             </a>
                         </li>
                         <li>
+                            <a href="#" class="nav-link text-secondary">
+                                <span class="fa-brands fa-github" style="width: 24px; height: 24px;"></span>
+                                GitHub
+                            </a>
+                        </li>
+                        <li>
                             <a href="#" class="nav-link text-white">
-                                <svg class="bi d-block mx-auto mb-1" width="24" height="24">
-                                    <use xlink:href="#people-circle" />
-                                </svg>
+                                <span class="fa-solid fa-info" style="width: 24px; height: 24px;"></span>
                                 About
                             </a>
                         </li>
@@ -52,14 +50,23 @@
             </div>
         </div>
     </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col col-lg-12">
+                    Something
+                </div>
+            </div>
+        </div>
+    </main>
     <?php
-    // $db = new SQLite3('tech_db.db');
-    // $results = $db->query('SELECT tech_name FROM technologies');
+        $db = new SQLite3('tech_db.db');
+        $results = $db->query('SELECT tech_name FROM technologies');
 
-    // while ($row = $results->fetchArray()) {
-    //     print($row['tech_name']);
-    //     print("<br/>");
-    // }
+        while ($row = $results->fetchArray()) {
+            print($row['tech_name']);
+            print("<br/>");
+        }
     ?>
 </body>
 <script>
