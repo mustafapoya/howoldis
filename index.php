c3196b1b636ad5440e219a4524d24786871e9158
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-11-12 14:57:01 +0430

    format header

--- index.php
+++ index.php
@@ -24,12 +24,12 @@
             <div class="container">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-                        <img src="library/img/golbarg.png" class="bi me-2" alt="golbarg logo" style="width: 40px; height: 40px;">
+                        <img src="library/img/golbarg.png" class="bi me-2" alt="golbarg logo" style="width: 34px; height: 34px;">
                         Golbarg.net
                     </a>
                     <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
                         <li>
-                            <a href="#" class="nav-link text-secondary">
+                            <a href="#" class="nav-link text-white">
                                 <span class="fa-solid fa-house-user" style="width: 24px; height: 24px;"></span>
                                 Home
                             </a>
@@ -41,7 +41,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="nav-link text-white">
+                            <a href="#" class="nav-link text-secondary">
                                 <span class="fa-solid fa-info" style="width: 24px; height: 24px;"></span>
                                 About
                             </a>
