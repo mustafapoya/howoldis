b3efb5e91db2fd55490a34a3d452b5fbea7d418c
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-11-13 16:38:01 +0430

    update links, style

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 
 <body>
     <div id="overlay">
-        <img src="library/img/loading.svg" style="width: 100px; height: 100px; left: 50%; top: 50%; position: relative;" alt="Loading" />
+        <img src="library/img/loading.svg" style="width: 60px; height: 60px; left: 50%; top: 50%; position: relative;" alt="Loading" />
     </div>
     <header>
         <div class="px-3 py-2 text-bg-dark">
@@ -28,23 +28,23 @@
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
                         <img src="library/img/golbarg.png" class="bi me-2" alt="golbarg logo" style="width: 34px; height: 34px;">
-                        Golbarg.net  
+                        Golbarg.net
                     </a>
                     <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
                         <li>
-                            <a href="#" class="nav-link text-white">
+                            <a href="index.php" class="nav-link text-white">
                                 <span class="fa-solid fa-house-user" style="width: 24px; height: 24px;"></span>
                                 Home
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="nav-link text-secondary">
+                            <a href="https://github.com/mustafapoya/howoldis" target="_blank" class="nav-link text-secondary">
                                 <span class="fa-brands fa-github" style="width: 24px; height: 24px;"></span>
                                 GitHub
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="nav-link text-secondary">
+                            <a href="about.php" class="nav-link text-secondary">
                                 <span class="fa-solid fa-info" style="width: 24px; height: 24px;"></span>
                                 About
                             </a>
@@ -68,8 +68,8 @@
                 <div class="col col-lg-12">
                     <div class="input-box">
                         <input id="input-search" type="text" class="form-control" placeholder="Search technology">
-                        <i class="fa fa-search"></i>                  
-                  </div>
+                        <i class="fa fa-search"></i>
+                    </div>
                 </div>
             </div>
             <div class="row" style="margin-top: 10px;">
@@ -79,11 +79,11 @@
                             <div class="tech-item" tech-name="<?php echo $row['tech_name'] ?>">
                                 <a href="<?php echo $row['link']; ?>" target="_blank" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
                                     <?php
-                                        $icon = $row['icon'];
+                                    $icon = $row['icon'];
 
-                                        if ($icon == '') {
-                                            $icon = "fa-regular fa-code";
-                                        }
+                                    if ($icon == '') {
+                                        $icon = "fa-regular fa-code";
+                                    }
                                     ?>
                                     <span class="<?php echo $icon ?> colored rounded-circle flex-shrink-0" style="width: 40px; height: 40px; font-size: 40px;"></span>
                                     <div class="d-flex gap-2 w-100 justify-content-between">
@@ -110,31 +110,31 @@
     </main>
 </body>
 <script>
-$(function() {
-    $("#overlay").fadeOut(500, function() {
-        $(".main-content").fadeIn(500);
-    });
-})
-
-$(document).ready(function() {
-    tech_list = $('.tech-list');
-
-    $('#input-search').on('input', function() {
-        input = $(this);
-        search = input.val().toLowerCase();
-
-        tech_list.find('.tech-item').each(function() {
-            element = $(this);
-            tech_name = element.attr('tech-name').toLowerCase();
-
-            if(tech_name.includes(search)) {
-                element.show();
-            } else {
-                element.hide();
-            }
-        })
+    $(function() {
+        $("#overlay").fadeOut(500, function() {
+            $(".main-content").fadeIn(500);
+        });
     })
-});
+
+    $(document).ready(function() {
+        tech_list = $('.tech-list');
+
+        $('#input-search').on('input', function() {
+            input = $(this);
+            search = input.val().toLowerCase();
+
+            tech_list.find('.tech-item').each(function() {
+                element = $(this);
+                tech_name = element.attr('tech-name').toLowerCase();
+
+                if (tech_name.includes(search)) {
+                    element.show();
+                } else {
+                    element.hide();
+                }
+            })
+        })
+    });
 </script>
 
 </html>
