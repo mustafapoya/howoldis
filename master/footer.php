960674624456b01bbabcfa20e7ced847ca327074
MustafaPoya <mustafapoyaabc@gmail.com>
2022-11-19 10:36:55 +0430

    create footer

---
+++ master/footer.php
@@ -0,0 +1,82 @@
+<div class="">
+    <!-- Footer -->
+    <footer class="text-center text-white" style="background-color: #3f51b5">
+        <!-- Grid container -->
+        <div class="container">
+            <!-- Section: Links -->
+            <section class="mt-5">
+                <!-- Grid row-->
+                <div class="row text-center d-flex justify-content-center pt-5">
+                    <!-- Grid column -->
+                    <div class="col-md-2">
+                        <h6 class="text-uppercase font-weight-bold">
+                            <a href="index.php" class="text-white">Main</a>
+                        </h6>
+                    </div>
+                    <!-- Grid column -->
+                    <!-- Grid column -->
+                    <div class="col-md-2">
+                        <h6 class="text-uppercase font-weight-bold">
+                            <a href="about.php" class="text-white">About us</a>
+                        </h6>
+                    </div>
+                    <!-- Grid column -->
+                    <!-- Grid column -->
+                    <div class="col-md-2">
+                        <h6 class="text-uppercase font-weight-bold">
+                            <a href="mailto:contact@golbarg.net" class="text-white">Contact</a>
+                        </h6>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row-->
+            </section>
+            <!-- Section: Links -->
+            <hr class="my-5" />
+
+            <!-- Section: Text -->
+            <section class="mb-5">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-lg-8">
+                        <p>
+                            If a technology is Missed? You can find this Repository on 
+                            <a href="https://github.com/mustafapoya/howoldis" class="text-white" target="_blank">
+                                <span class="devicon-github-original colored"></span>Github
+                            </a>.
+                            If you want to contact me send an 
+                            <a href="mailto:mustafapoyaabc@gmail.com" class="text-white" target="_blank">
+                                <i class="fa-solid fa-envelope"></i>
+                                Email
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <!-- Section: Text -->
+
+            <!-- Section: Social -->
+            <section class="text-center mb-5">
+                <a href="https://golbarg.net/mp_profile" class="text-white me-4">
+                    <i class="fa-solid fa-user"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/mustafapoyaabc" class="text-white me-4">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                <a href="http://github.com/mustafapoya" class="text-white me-4">
+                    <i class="fab fa-github"></i>
+                </a>
+            </section>
+            <!-- Section: Social -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            © 2020 Copyright:
+            <a class="text-white" href="https://golbarg.net/">golbarg.net</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+</div>
+<!-- End of .container -->
